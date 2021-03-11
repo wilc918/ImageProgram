46b5caad861ec54533b79022a2cb7ec2303c0abd
wilc918 <72207598+wilc918@users.noreply.github.com>
2021-03-11 13:26:39 +0000

    Removed Redundant Delegate

--- ImageProgram/Delegates.cs
+++ ImageProgram/Delegates.cs
@@ -7,12 +7,6 @@ using System.Threading.Tasks;
 namespace ImageProgram
 {
     /// <summary>
-    /// Declare a delegate for selecting an item, call it SelectItem
-    /// </summary>
-    /// <param name="key">Key that identifies selection</param>
-    public delegate void SelectItemDelegate(int key);
-
-    /// <summary>
     /// Declare a delegate for retrieving listed of imageName, call it RetrieveImageList
     /// </summary>
     /// <param name="noteKey">Note Identifier</param>
